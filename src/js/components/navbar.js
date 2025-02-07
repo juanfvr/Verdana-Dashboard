898ccd3d064c947b33a38b041be898a5be8e4424
Juan Ferreira <juanferreiraverao@outlook.com>
2025-02-07 18:03:38 -0400

    navbar hamburguer

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,5 +1,25 @@
 const navButton = document.querySelector('#nav-button');
+const navSidebar = document.querySelector('#nav-sidebar');
+
 
 navButton.addEventListener('click', () => {
-    console.log('a')
+    navSidebar.classList.remove('hidden')
 })
+
+const closeSidebar = document.querySelector('#close-sidebar');
+
+closeSidebar.addEventListener('click', () => {
+    navSidebar.classList.add('hidden');
+})
+
+function fecharNavbar() {
+    navSidebar.classList.add('hidden')
+}
+
+const navLinks = document.querySelector('a');
+
+document.querySelector("#navSidebar").addEventListener("click", (event) => {
+    if (event.target.classList.contains("nav-item")) {
+        fecharNavbar();
+    }
+});
